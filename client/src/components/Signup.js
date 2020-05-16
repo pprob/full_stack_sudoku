b8f683c786e24f739343706b9b2d9ddfe1d82e36
unknown <p.probert340@gmail.com>
2020-05-16 18:51:11 +0100

    feats

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -6,13 +6,14 @@ import {
   setPasswordField,
 } from "../redux/actions/signupActions";
 import "../styles/Signup.css";
-import axios from 'axios'
+import axios from "axios";
 
 //state for email/username/password
 
 const Signup = (props) => {
   const { signupState, dispatch } = props;
-  console.log(signupState);
+  const { error, email, password, username } = signupState;
+
   const setEmail = (e) => {
     dispatch(setEmailField(e.target.value));
   };
@@ -22,42 +23,62 @@ const Signup = (props) => {
   const setPassword = (e) => {
     dispatch(setPasswordField(e.target.value));
   };
-  const onSubmit = (e) => {
-    e.preventDefault()
-  }
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const requestField = {
+      email: email,
+      username: username,
+      password: password,
+    };
+    console.log(requestField);
+
+    const response = await axios.post(
+      "http://localhost:5000/api/users/register",
+      requestField
+    );
+    console.log(response);
+  };
 
   return (
-    <div className='app-body'>
-      <div className='app-container'>
-        <div className='header'>
+    <div className="app-body">
+      <div className="app-container">
+        <div className="header">
           <h1>Welcome - It's good to see you</h1>
           <h2>Let's get you signed in</h2>
         </div>
-        <div className='form-container'>
+        <div className="form-container">
           <form onSubmit={onSubmit}>
             <label>Email address</label>
             <input
-              className='field'
-              placeholder='e.g abc@hotmail.com'
+              className="field"
+              placeholder="e.g abc@hotmail.com"
               value={signupState.email}
               onChange={setEmail}
             ></input>
             <label>Username</label>
             <input
-              className='field'
-              placeholder='Choose your user id. e.g sudokumeister1337'
+              className="field"
+              placeholder="Choose your user id. e.g sudokumeister1337"
               value={signupState.username}
               onChange={setUsername}
             ></input>
             <label>Password</label>
             <input
-              type='password'
-              className='field'
-              placeholder='Must be minimum 8 characters length'
+              type="password"
+              className="field"
+              placeholder="Must be minimum 8 characters length"
               value={signupState.password}
               onChange={setPassword}
             ></input>
-            <button type='submit' className='submit-button'>Sign up!</button>
+            {error && (
+              <div className="error-container">
+                <label>{error}</label>
+              </div>
+            )}
+            <button type="submit" className="submit-button">
+              Sign up!
+            </button>
           </form>
         </div>
       </div>
