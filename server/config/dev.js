d47278792fe1752eed57e75fade06e86e6462b16
unknown <p.probert340@gmail.com>
2020-05-17 17:33:59 +0100

    security update

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,4 @@
+const config = require('./config')
 module.exports = {
-  mongoURI: 'mongodb+srv://pprob:pprob340@sudoku-test-sn76m.azure.mongodb.net/test?retryWrites=true&w=majority'
+  mongoURI: config.mongoURI
 }
