bb2f8544ea1cafa1cef74a04c6c152f45855f017
unknown <p.probert340@gmail.com>
2020-05-31 21:19:36 +0100

    updates

--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import App from '../components/App'
-import Banner from '../components/Banner'
-import Footer from '../components/Footer'
-import Signup from '../components/Signup'
-import Login from '../components/Login'
-import '../styles/App.css'
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import App from "../components/App";
+import Banner from "../components/Banner";
+import Footer from "../components/Footer";
+import Signup from "../components/Signup";
+import Login from "../components/Login";
+import "../styles/App.css";
+
+const AppRouter = () => {
+  // useEffect call auth get route, if successful setState (have to use redux to dispatch to store) auth == true, pass auth prop down to Banner (navbar)
+  // axios.get(/api/users/auth, withCredentials: true)
 
 
-const AppRouter = () => (
-  <BrowserRouter>
-    <div className='app'>
-      <Banner />
-      <Switch>
-        <Route exact path='/' component={App} />
-        <Route path='/signup' component={Signup}/>
-        <Route path='/login' component={Login}/>
-      </Switch>
-      <Footer />
-    </div>
-  </BrowserRouter>
-)
+  useEffect(() => {
+    console.log('loaded')
+  })
 
-export default AppRouter
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Banner />
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+        </Switch>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+};
 
+export default AppRouter;
