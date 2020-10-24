d9c470e8685f06a9c2ab43228f8b425d4ad4dee4
unknown <p.probert340@gmail.com>
2020-10-25 00:35:30 +0100

    feat: added new labels

--- client/src/data/labels.js
+++ client/src/data/labels.js
@@ -1,4 +1,5 @@
 export default {
   emailErrorField: "Please use a valid email address",
-  passwordContainsError: "Your password cannot contain the word password"
+  passwordContainsError: "Your password cannot contain the word password",
+  TakeToLoginPage: "Taking you to the Login page"
 }
