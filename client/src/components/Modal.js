bc5d849f9f9b3af488641a66fcfcf8b2a02c995b
unknown <p.probert340@gmail.com>
2020-10-25 00:33:43 +0100

    feat: added modal component

---
+++ client/src/components/Modal.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../styles/Modal.css";
+
+const Modal = ({
+  title,
+  description,
+  onButtonOneClick,
+  buttonOneLabel,
+  buttonTwoLabel,
+  onButtonTwoClick,
+  onCloseAction,
+  children,
+}) => {
+  const renderButtonContainer = () => {
+    return (
+      <div className="modal-button-container">
+        {onButtonOneClick && <button className="modal-button" onClick={onButtonOneClick}>{buttonOneLabel}</button>}
+        {onButtonTwoClick && <button className="modal-button" onClick={onButtonTwoClick}>{buttonTwoLabel}</button>}
+      </div>
+    );
+  };
+  return (
+    <div className="modal-container">
+      <div className="modal-content">
+        <div className="modal-header-container">
+          <h1 className="modal-title">Success!</h1>
+        </div>
+        <hr style={{ width: "80%" }} />
+        <div className="modal-main-content">{children}</div>
+        {onButtonOneClick || onButtonTwoClick ? renderButtonContainer() : null}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
